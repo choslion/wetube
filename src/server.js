65d188759e5e8afcf61fce34217e781c9fb1dead
choslion <cshyeok24@gmail.com>
2022-12-02 00:09:17 +0900

    finished chapter3-2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 // const express = require("express");
 import express from "express"
 // sexy code
+const PORT = 4000;
 
 const app = express();
 
-console.log("i m good!");
+app.get("/", (req,res) => {
+  console.log(req.url, req.method);
+  res.end('Hello node');
+})
+
+const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
