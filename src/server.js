06c795ffbacf141da625e62d7a992e3ef81a0c1a
choslion <cshyeok24@gmail.com>
2022-12-04 17:42:14 +0900

    finished middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 // const express = require("express");
 import express from "express"
+import morgan from "morgan"
 // sexy code
 const PORT = 4000;
 
 const app = express();
+// middleware express morgan
+const logger = morgan("dev");
 
-app.get("/", (req,res) => {
-  console.log(req.url, req.method);
-  res.end('Hello node');
-})
+const login = (req,res) => {
+  console.log("logged in!");
+  res.send("you've logged in!");
+}
+
+const home = (req,res) => {
+  res.send("you are in home");
+}
+
+app.use(logger);
+app.get("/login", login);
+app.get("/", home);
+
 
 const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
