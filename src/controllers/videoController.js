5e6427be1fd8fb90affc14a7e2f13899a5d54b50
choslion <cshyeok24@gmail.com>
2022-12-13 21:01:53 +0900

    async ,await 개념  , find()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,57 +1,27 @@
-const fakeUser = {
-  username: "eddy",
-  loggedIn: true,
+import Video from "../models/Video";
+
+Video.find({}, (err, videos) => {});
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "10 minutes ago",
-    views: 57649,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 4,
-    comments: 2,
-    createdAt: "5 minutes ago",
-    views: 549,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 12,
-    createdAt: "2 minutes ago",
-    views: 10239,
-    id: 3,
-  },
-];
-
-export const trendVideos = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos: videos });
-};
 export const search = (req, res) => res.send("search");
 export const watch = (req, res) => {
   const { id } = req.params;
   // 위랑 같음
   // const id = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, id, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   // const title = req.body.title;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 export const upload = (req, res) => res.send("Upload");
@@ -66,14 +36,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,14 +1,14 @@
 // 각 모듈에 express를 호출해줘야 쓸수있다.
-import express from "express"
-import {login,join} from "../controllers/userController";
-import { trendVideos,search } from "../controllers/videoController";
+import express from "express";
+import { login, join } from "../controllers/userController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trendVideos);
-globalRouter.get("/join" ,join);
-globalRouter.get("/login",login);
-globalRouter.get("/search",search);
+globalRouter.get("/", home);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 // server.js 에서 쓸 수 있게 모듈화 시켜서 내보내준다.
 // 모든파일은 독립적이다.
