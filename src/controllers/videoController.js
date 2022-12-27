1bf7a7b28580e05afae2e7f57953b0aee92fbc41
choslion <cshyeok24@gmail.com>
2022-12-27 21:23:01 +0900

    model 에서 사용할수있는 pre 미들웨어

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -40,14 +40,15 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.findById(id);
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -67,8 +68,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      createdAt: { type: Date, required: true, default: Date.now },
-      hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags,
       // meta: {
       //   views: 0,
       //   rating: 0,
--- src/models/Video.js
+++ src/models/Video.js
@@ -13,6 +13,10 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0].split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 const Video = mongoose.model("Video", videoSchema);
 // "파일명" , 문서구조 변수로담은것.
 export default Video;
