0322f5465f83a55c4cbbd2decba9aa8bfb7788fa
choslion <cshyeok24@gmail.com>
2023-01-19 21:27:49 +0900

    8.0 프로필수정 페이지를 만들고 수정버튼을 눌렀을때 로그인정보를 받아오게만듬.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -122,5 +122,10 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
-export const edit = (req, res) => res.send("Edit user");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile");
+};
 export const see = (req, res) => res.send("See User");
