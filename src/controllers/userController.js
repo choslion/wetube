56e9a4602116af74b1508917bf64fa6caeb30bfe
choslion <cshyeok24@gmail.com>
2023-01-19 20:49:21 +0900

    7.22 로그아웃 구현 , avatarUrl 데이터 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -35,7 +35,7 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) => res.render("login", { pageTitle: "LogIn" });
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", { pageTitle: "LogIn", errorMessage: "An account with this username does not exists" });
   }
@@ -86,7 +86,7 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    // console.log(userData);
+    console.log(userData);
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
@@ -94,18 +94,15 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    console.log(emailData);
+    // console.log(emailData);
     const emailObj = emailData.find((email) => email.primary === true && email.verified === true);
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -113,15 +110,17 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.location,
       });
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
 };
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit user");
-export const remove = (req, res) => res.send("Remove user");
-export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
