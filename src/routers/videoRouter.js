f5fef31d555d19f9c86ec33f63718464f6cb61ce
choslion <cshyeok24@gmail.com>
2023-01-21 23:10:10 +0900

    8.2 프로필 수정 , ES6 와  mongoose 를 이용해 작업함.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { watch, getEdit, postEdit, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").all(protectorMiddleware).get(deleteVideo);
+videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(postUpload);
 
 // 하나의 url에 여러개써줄때 유용.
 // 위에 한줄과 같아짐. get일떄는 getEdit를 불러주고 post 일때는 postEdit함수를 가져온다.
