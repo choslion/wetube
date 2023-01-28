f71e031fbdd5df42a14da293bec65ab5f1daf824
choslion <cshyeok24@gmail.com>
2023-01-28 20:53:51 +0900

    11.1 play버튼 활성화. block 안에 block 스크립트를 넣으면 작동이 안된다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("video palyer");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    console.log("hello");
+    video.play();
+    console.log("hi");
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
