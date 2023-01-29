71b7f5f5a8215526cb8a68b606a329a2fe2e0e20
choslion <cshyeok24@gmail.com>
2023-01-29 21:28:07 +0900

    13.4 video 파일형식을 정해주고 다운받을 수 있게  a태그만들고 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,30 +1,46 @@
+import { Body } from "node-fetch";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
 };
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = (e) => console.log(e);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
   recorder.start();
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: { width: 200, height: 200 },
+    audio: false,
+    video: true,
   });
   video.srcObject = stream;
   video.play();
